1511775d9a3561d7d859fc1264623db020e73785
Maxim Prav <mxmlknv@mail.ru>
2023-03-04 16:06:38 +0300

    Задача со сменой столбцов и строк

---
+++ Task55/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, 
+// которая заменяет строки на столбцы. В случае, если это невозможно, 
+// программа должна вывести сообщение для пользователя.
+
+Console.Clear();
+Console.WriteLine("Введите количество строк массива:");
+int rows = int.Parse(Console.ReadLine()!)!;
+Console.WriteLine("Введите количество столбцов массива:");
+int columns = int.Parse(Console.ReadLine()!)!;
+
+int[,] array = GetArray(rows, columns, 1, 10);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Поменяем местами строки и столбцы:");
+Console.WriteLine();
+int[,] array88 = ChangeArray(array);
+if (rows == columns)
+{
+    PrintArray(array88);
+}
+else Console.WriteLine($"Невозможно заменить строки и столбцы");
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ChangeArray(int[,] inArray)
+{
+    int[,] newArray = new int[inArray.GetLength(1), inArray.GetLength(0)];
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            newArray[j, i] = inArray[i, j];
+        }
+    }
+    return newArray;
+}
